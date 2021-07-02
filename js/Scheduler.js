9af84d4783e12f252fe4134738083bf5654fd9ab
Edwin Eefting <edwin@datux.nl>
2021-07-02 22:40:58 +0200

    bla

---
+++ js/Scheduler.js
@@ -0,0 +1,42 @@
+class Interval {
+    constructor(interval, time, repeat, callback) {
+        this.interval = interval;
+        this.repeat = repeat;
+        this.nextTime = time + interval;
+        this.callback = callback;
+    }
+    check(time) {
+        if (time >= this.nextTime) {
+            this.nextTime = time + this.interval;
+            if (this.repeat) {
+                this.repeat = this.repeat - 1;
+                this.callback(time);
+                if (!this.repeat)
+                    return false; //removes interval-instance
+            }
+            else {
+                this.callback(time);
+            }
+            return (true);
+        }
+    }
+}
+export class Scheduler {
+    constructor() {
+        this.frameNr = 0;
+        this.intervals = [];
+    }
+    interval(frames, callback) {
+        const interval = new Interval(frames, this.frameNr, 0, callback);
+        this.intervals.push(interval);
+        return (interval);
+    }
+    //called by matrix on every frame.
+    update() {
+        this.frameNr++;
+        for (let i = 0, n = this.intervals.length; i < n; ++i) {
+            this.intervals[i].check(this.frameNr);
+        }
+    }
+}
+//# sourceMappingURL=Scheduler.js.map
---
+++ js/scheduler.js
@@ -0,0 +1,6 @@
+import { MatrixCanvas } from "./MatrixCanvas.js";
+import { AnimationTest } from "./AnimationTest.js";
+let matrix = new MatrixCanvas(37, 8, '#matrix', 5, 16);
+matrix.addAnimation(new AnimationTest());
+matrix.run();
+//# sourceMappingURL=scheduler.js.map
