3076026788d9bc1a00b767cd73732a92803cd030
Edwin Eefting <edwin@datux.nl>
2021-07-12 22:04:24 +0200

    very nice

--- js/ControlColor.ts
+++ js/ControlColor.ts
@@ -1,17 +1,57 @@
 import {Control} from "./Control.js";
 import {ColorInterface} from "./ColorInterface.js";
+import iro from "@jaames/iro";
+import ColorPicker = iro.ColorPicker;
 
 export class ControlColor extends Control implements ColorInterface {
-    r: number;
-    g: number;
-    b: number;
-    a: number;
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+  jqueryElement: JQuery;
 
-    constructor(name: string, r: number, g: number, b: number, a: number=1) {
-        super( name);
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.a = a;
-    }
+  constructor(name: string, r: number, g: number, b: number, a: number = 1) {
+    super(name);
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = a;
+  }
+
+
+  html(container: HTMLElement) {
+
+    this.jqueryElement = $(`
+     <div class="ui padded  segment ">
+      <div class="ui top attached label">${this.name}</div>
+      <div class="colorpicker"></div>
+      <button class="ui icon button">
+        <i class="undo icon"></i>
+      </button>
+    </div>
+   `);
+
+    $(container).append(this.jqueryElement);
+
+    const colorElement=$('.colorpicker', this.jqueryElement)[0];
+
+    const picker=ColorPicker(colorElement,{
+      color: this,
+    });
+
+    picker.on("color:change", (color)=> {
+      this.r=color.rgba.r;
+      this.g=color.rgba.g;
+      this.b=color.rgba.b;
+      this.a=color.rgba.a;
+
+    });
+  }
+
+  destroy() {
+    if (this.jqueryElement !== undefined)
+      this.jqueryElement.remove();
+  }
+
+
 }
