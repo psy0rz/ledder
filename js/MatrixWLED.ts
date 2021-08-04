c2f7833ee7adac26f7a35d9e670f0112f261c22d
Edwin Eefting <edwin@datux.nl>
2021-08-04 13:21:41 +0200

    fix

--- js/MatrixWLED.ts
+++ js/MatrixWLED.ts
@@ -21,7 +21,7 @@ let gamma = [
   177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
   215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 ]
 
-//Matrix driver for WLED https://github.com/Aircoookie/WLED/wiki/UDP-Realtime-Control
+//Matrix driver for WLED https://github.com/Aircoookie/WLED/wiki/UDP-Realtime-Control via DRGB
 export class MatrixWLED extends Matrix {
 
   buffer: Uint8ClampedArray;
