6f8ede38541a9beb86993061419d75f8a9623744
Edwin Eefting <edwin@datux.nl>
2021-08-05 17:10:28 +0200

    autoupload for raspberry

---
+++ autoupload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IP=$1
+
+[ "$IP" ] || exit 1
+
+STUFF="js index.html dist"
+
+find $STUFF|entr -r -s "rsync -avx $STUFF root@$IP:ledder && ssh root@$IP 'pkill node ;cd ledder && node js/server.js'"
+
