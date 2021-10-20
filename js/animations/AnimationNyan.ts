7bc8412b0a0f28d814f7ba02fa6d66611af08bcd
Edwin Eefting <edwin@datux.nl>
2021-10-20 21:21:47 +0200

    Revert "bla"
    This reverts commit e75b7e2d72d5b58b4313d50935d71801dd16926c.

--- js/animations/AnimationNyan.ts
+++ js/animations/AnimationNyan.ts
@@ -28,8 +28,7 @@ export class AnimationNyan
 
     new AnimationMovingStarsL(matrix);
 
-
-    const body_str=`
+    const body = new AnimationAsciiArt(matrix, 6, 7, `
       .00000000.
       0ffffpfff0
       0fpffffff0
@@ -37,9 +36,9 @@ export class AnimationNyan
       0ffffffff0
       0fpffpfff0
       .00000000.
-    `
+    `)
 
-    const head_str=`
+    const head = new AnimationAsciiArt(matrix, 12, 8, `
       .00...00.
       .0500050.
       05w05w050
@@ -47,41 +46,19 @@ export class AnimationNyan
       0p55555p0
       .0555550.
       ..00000..
-    `
+    `)
 
 
     const moveIntervalControl = matrix.preset.value("Nyan move interval", 3, 1, 20, 0.1);
     const wobbleIntervalControl = matrix.preset.value("Nyan wobble interval", 15, 1, 20, 0.1);
 
 
-
-
-    const mover=new AnimationMove(matrix, moveIntervalControl, {value: 1}, {value: 0}, true)
-
-    let o=0;
-    function body() {
-
-      const body = new AnimationAsciiArt(matrix, 6, 7, body_str)
-      const wobblyY = new AnimationWobbleY(matrix, {value: 1}, wobbleIntervalControl, o)
-      o=o+10
-      wobblyY.addPixels(body.pixels)
-      mover.addPixels(body.pixels)
-      return(body)
-    }
-    body();
-
-    const head = new AnimationAsciiArt(matrix, 12, 8, head_str)
+    new AnimationWobbleY(matrix, {value: 1}, wobbleIntervalControl, 0).addPixels(body.pixels)
     new AnimationWobbleY(matrix, {value: -1}, wobbleIntervalControl, wobbleIntervalControl.value*1/2).addPixels(head.pixels)
     new AnimationWobbleX(matrix, {value: 1}, wobbleIntervalControl, wobbleIntervalControl.value*1/3).addPixels(head.pixels)
-    mover.addPixels(head.pixels);
 
+    new AnimationMove(matrix, moveIntervalControl, {value: 1}, {value: 0}, true).addPixels(head.pixels).addPixels(body.pixels)
 
-    let l=0;
-    matrix.scheduler.interval(30, () => {
-      l=l+1;
-      if (l<8)
-        body();
-    })
 
     //rainbow :)
     let x = 6;
@@ -112,7 +89,7 @@ export class AnimationNyan
 
       for (let c = 0; c < 6; c++) {
         new AnimationFadeOut(matrix, colors[c], controlFade, controlFadeRnd, true)
-          .addPixel(new Pixel(matrix, x-70, c + y + 1, colors[c]))
+          .addPixel(new Pixel(matrix, x, c + y + 1, colors[c]))
       }
 
     })
