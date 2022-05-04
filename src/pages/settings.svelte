5613824fa9ee5355494ced4aa463e9969653d278
Edwin Eefting <edwin@datux.nl>
2022-05-04 23:42:59 +0200

    tree

--- src/pages/settings.svelte
+++ src/pages/settings.svelte
@@ -26,108 +26,119 @@
                     rpc.request("runner.runName", "PoliceLights", "");
 
 
-                 } } />
+                 } }/>
             </Menu>
 
         </Subnavbar>
 
     </Navbar>
 
+    <Block strong>
+        <Treeview>
+            {#each $sveltePresets as preset, i (preset.meta.name)}
+                <TreeviewItem label="{preset.meta.name}" opened toggle={true} itemToggle>
+                    <TreeviewItem opened toggle={false}>
 
-    {#each $sveltePresets as preset, i (preset.meta.name)}
-        <BlockTitle>{preset.meta.name}</BlockTitle>
-        <Block inset>
-            {#if preset.meta.type === 'value' }
-                <Stepper
-                        bind:value={preset.value}
-                        min={preset.meta.min}
-                        max={preset.meta.max}
-                        step={preset.meta.step}
-                />
-                <Range bind:value={preset.value}
-                       min={preset.meta.min}
-                       max={preset.meta.max}
-                       step={preset.meta.step}
-                       scaleSteps={5}
-                       scaleSubSteps={5}
-                       scale
-                       label={true}
-                       on:rangeChange={ e=> {
-                           preset.value=e.detail[0]
-                           rpc.notify("matrix.control.updateValue", preset.meta.name, preset)
-                       } }/>
-            {:else if preset.meta.type === 'color'}
-                <div style="max-width: 200px" id="color-picker-{i}"></div>
-                <Input
-                        type="colorpicker"
-                        label="Color Wheel"
-                        placeholder="Color"
-                        readonly
-                        value={{
-                            rgb: [preset.r, preset.g, preset.b],
-                            alpha: preset.a
-                        }}
+                  <span slot="content" class="padding-bottom">
+                        {#if preset.meta.type === 'value' }
 
-                        colorPickerParams={{
-                            containerEl: "#color-picker-"+i,
-                            modules: ["wheel", "alpha-slider"],
-                            on: {
-                                change: (e,c)=>{
-                                    preset.r=c.rgb[0]
-                                    preset.g=c.rgb[1]
-                                    preset.b=c.rgb[2]
-                                    preset.a=c.alpha
+                        <Stepper
+                                bind:value={preset.value}
+                                min={preset.meta.min}
+                                max={preset.meta.max}
+                                step={preset.meta.step}
+                        />
+                        <Range bind:value={preset.value}
+                               min={preset.meta.min}
+                               max={preset.meta.max}
+                               step={preset.meta.step}
+                               scaleSteps={5}
+                               scaleSubSteps={5}
+                               scale
+                               label={true}
+                               style="width:400px"
+                               on:rangeChange={ e=> {
+                                   preset.value=e.detail[0]
+                                   rpc.notify("matrix.control.updateValue", preset.meta.name, preset)
+                               } }/>
+                        {:else if preset.meta.type === 'color'}
+                            <div style="max-width: 200px" id="color-picker-{i}"></div>
+                            <Input
+                                    type="colorpicker"
+                                    label="Color Wheel"
+                                    placeholder="Color"
+                                    readonly
+                                    value={{
+                                    rgb: [preset.r, preset.g, preset.b],
+                                    alpha: preset.a
+                                }}
 
+                                    colorPickerParams={{
+                                    containerEl: "#color-picker-"+i,
+                                    modules: ["wheel", "alpha-slider"],
+                                    on: {
+                                        change: (e,c)=>{
+                                            preset.r=c.rgb[0]
+                                            preset.g=c.rgb[1]
+                                            preset.b=c.rgb[2]
+                                            preset.a=c.alpha
+
+                                            rpc.notify("matrix.control.updateValue", preset.meta.name, preset)
+                                        }
+                                    }
+                                }}
+
+                            />
+                        {:else if preset.meta.type === 'input'}
+                            <Input type="textarea" value={preset.text}
+                                   on:input={ (e)=>{
+                                    preset.text=e.detail[0].target.value
                                     rpc.notify("matrix.control.updateValue", preset.meta.name, preset)
-                                }
-                            }
-                        }}
-
-                />
-            {:else if preset.meta.type === 'input'}
-                <Input type="textarea" value={preset.text}
-                       on:input={ (e)=>{
-                            preset.text=e.detail[0].target.value
+                               }}
+                            />
+                        {:else if preset.meta.type === 'switch'}
+                            <Toggle checked={preset.enabled} on:toggleChange={ (e)=>
+                        {
+                            preset.enabled=e.detail[0]
                             rpc.notify("matrix.control.updateValue", preset.meta.name, preset)
-                       }}
-                />
-            {:else if preset.meta.type === 'switch'}
-                <Toggle checked={preset.enabled} on:toggleChange={ (e)=>
-                {
-                    preset.enabled=e.detail[0]
-                    rpc.notify("matrix.control.updateValue", preset.meta.name, preset)
-                }}/>
-            {:else if preset.meta.type === 'select'}
-                <Input
-                        type="select"
-                        label="Select"
-                        value={ preset.selected }
-                        on:input={ (e)=>{
-                            console.log(e)
-                            preset.selected=e.detail[0].target.value
-                            rpc.notify("matrix.control.updateValue", preset.meta.name, preset)
-                       }}
+                        }}/>
+                        {:else if preset.meta.type === 'select'}
+                            <Input
+                                    type="select"
+                                    label="Select"
+                                    value={ preset.selected }
+                                    on:input={ (e)=>{
+                                    console.log(e)
+                                    preset.selected=e.detail[0].target.value
+                                    rpc.notify("matrix.control.updateValue", preset.meta.name, preset)
+                               }}
 
-                >
-                    {#each preset.meta.choices as choice}
-                        <option value="{choice.id}">{choice.name}</option>
-                    {/each}
-                </Input>
+                            >
+                                {#each preset.meta.choices as choice}
+                                    <option value="{choice.id}">{choice.name}</option>
+                                {/each}
+                            </Input>
 
+                        {:else}
+                            <b>Unknown control type: {preset.meta.name} has type {preset.meta.type} !</b>
+                        {/if}
+                    </span>
+                    </TreeviewItem>
+                </TreeviewItem>
             {:else}
-                <b>Unknown control type: {preset.meta.name} has type {preset.meta.type} !</b>
-            {/if}
-        </Block>
-    {:else}
-        leeg??
-    {/each}
+                leeg??
+            {/each}
 
+        </Treeview>
+    </Block>
 </Page>
 <script>
     import {
         Page,
         Navbar,
         BlockTitle,
+        Treeview,
+        TreeviewItem,
         Range,
         Block, Input, Stepper, Menu, MenuItem, Subnavbar, Toggle,
     } from 'framework7-svelte';
