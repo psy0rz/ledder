099d74bf1e1b5402e14bda11161d04a7ef56d1d5
Edwin Eefting <edwin@datux.nl>
2022-05-18 16:50:01 +0200

    wip

--- src/components/ControlControls.svelte
+++ src/components/ControlControls.svelte
@@ -1,13 +1,15 @@
 {#each Object.values(controls.meta.controls) as control, i (control.meta.name)}
     {#if control.meta.type === 'controls'}
-        <TreeviewItem label="{control.meta.name}" opened={true} toggle={true} itemToggle>
+        <TreeviewItem label="{control.meta.name}" opened={true} toggle={true} itemToggle iconMaterial="folder">
             <svelte:self controls={control}/>
         </TreeviewItem>
     {:else}
-        <TreeviewItem opened toggle={false} label="{control.meta.name}">
+        <TreeviewItem opened toggle={false} >
+
         <span slot="content" class="padding-bottom">
+            <BlockHeader class="">{control.meta.name}:</BlockHeader>
             {#if control.meta.type === 'value' }
-                valueding
+                <ControlValue control={control}/>
             {/if}
         </span>
         </TreeviewItem>
@@ -15,7 +17,9 @@
 {/each}
 
 <script>
+    import ControlValue from "./ControlValue.svelte";
+
     export let controls;
-    import {TreeviewItem} from "framework7-svelte";
+    import {BlockHeader, BlockTitle, TreeviewItem} from "framework7-svelte";
 
 </script>
---
+++ src/components/ControlValue.svelte
@@ -0,0 +1,27 @@
+<Stepper
+        bind:value={control.value}
+        min={control.meta.min}
+        max={control.meta.max}
+        step={control.meta.step}
+/>
+<Range bind:value={control.value}
+       min={control.meta.min}
+       max={control.meta.max}
+       step={control.meta.step}
+       scaleSteps={5}
+       scaleSubSteps={5}
+       scale
+       label={true}
+       style="width:400px"
+       on:rangeChange={ e=> {
+                                   //control.value=e.detail[0]
+                                   //rpc.notify("matrix.control.updateValue", control.meta.name, preset)
+                               } }/>
+
+<script>
+    import {Range, Stepper} from "framework7-svelte";
+    import {rpc} from "../js/web/RpcClient.js";
+
+    export let control;
+
+</script>
