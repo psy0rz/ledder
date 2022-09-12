0e2312c4598df72ebea69f7f7cbee6485ca56dd0
Edwin Eefting <edwin@datux.nl>
2022-09-12 18:22:01 +0200

    bla

--- src/pages/display.svelte
+++ src/pages/display.svelte
@@ -1,34 +1,69 @@
-    <canvas class="ledder-display ledder-display-box" id='ledder-display' ></canvas>
-
-    <svg xmlns="http://www.w3.org/2000/svg" class='ledder-display-box' id='ledder-grid'>
-        <defs>
-            <pattern id="ledder-grid-pattern" width="{zoom}" height="{zoom}" patternUnits="userSpaceOnUse">
-            <path d="M {zoom} 0 L 0 0 0 {zoom}" fill="none" stroke="black" stroke-width="1"/>
-            </pattern>
-        </defs>
-            
-        <rect width="100%" height="100%" fill="url(#ledder-grid-pattern)" />
-    </svg>
-
-    <div class='ledder-display-box' style='color: white; text-align:right'>
-        {width} x {height}
-    </div>
-
 <script>
-    import {onMount} from 'svelte';
-    import {runnerBrowser} from "../js/web/RunnerBrowser.js";
+    import { onMount } from "svelte";
+    import { runnerBrowser } from "../js/web/RunnerBrowser.js";
+    import {
 
-    import {f7ready} from "framework7-svelte";
+Link,        Page,
+        Navbar,
+        Button,
+        BlockTitle,
+        Treeview,
+        List,
+        Popover,
+        TreeviewItem,
+        Range,
+        ListItem,
+        Block,
+        Input,
+        Stepper,
+        Menu,
+        MenuItem,
+        Subnavbar,
+        Toggle,
+        Checkbox,
+    } from "framework7-svelte";
 
+    import { f7ready } from "framework7-svelte";
 
-    let zoom=8;
-    let width=75;
-    let height=8;
+    let zoom = 8;
+    let width = 75;
+    let height = 8;
 
     onMount(async () => {
         f7ready(async () => {
-            await runnerBrowser.init()
-            await runnerBrowser.setSize(width, height, zoom)
-        })
-    })
+            await runnerBrowser.init();
+            await runnerBrowser.setSize(width, height, zoom);
+        });
+    });
 </script>
+
+<canvas class="ledder-display ledder-display-box" id="ledder-display" />
+
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="ledder-display-box"
+    id="ledder-grid"
+>
+    <defs>
+        <pattern
+            id="ledder-grid-pattern"
+            width={zoom}
+            height={zoom}
+            patternUnits="userSpaceOnUse"
+        >
+            <path
+                d="M {zoom} 0 L 0 0 0 {zoom}"
+                fill="none"
+                stroke="black"
+                stroke-width="1"
+            />
+        </pattern>
+    </defs>
+
+    <rect width="100%" height="100%" fill="url(#ledder-grid-pattern)" />
+</svg>
+
+<div class="ledder-display-box" style="color: white; text-align:right">
+    {width} x {height}
+</div>
+
