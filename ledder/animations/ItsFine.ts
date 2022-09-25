17cfaa90d1e763ccdb6322c87c663dcf1b08d77f
Edwin Eefting <edwin@datux.nl>
2022-09-25 17:49:45 +0200

    fixes

--- ledder/animations/ItsFine.ts
+++ ledder/animations/ItsFine.ts
@@ -7,8 +7,8 @@ import {fontSelect} from "../fonts.js";
 import PixelSet from "../PixelSet.js";
 import FxFlames from "../fx/FxFlames.js";
 
-import BrainsmokeFire from "./BrainsmokeFire.js";
 import PixelBox from "../PixelBox.js"
+import Fire from "./Fire.js"
 
 
 export default class ItsFine extends Animation {
@@ -19,29 +19,29 @@ export default class ItsFine extends Animation {
 
     async run(box: PixelBox, scheduler: Scheduler, controls: ControlGroup) {
 
-        // new BertrikFire().run(display, scheduler, controls.group("Bottom fire"))
-        new BrainsmokeFire().run(box, scheduler, controls.group("Bottom fire"))
-        controls.group("Bottom fire").value("Fire maximum intensity").value = 0
+
+        new Fire().run(box, scheduler, controls.group("Bottom fire"))
 
         const inputControl=controls.input("Text", "its fine.", true)
 
         const colorControl = controls.color("Text color", 0, 255, 0)
         const text = new DrawText(0, 0, fontSelect(controls), inputControl.text, colorControl).centerH(box)
         box.add(text)
+        text.center(box)
 
-        await scheduler.delay(60 * 2)
+        await scheduler.delay(60 * 4)
         const flames = new PixelSet()
         box.add(flames)
         new FxFlames(scheduler, controls.group("Top fire")).run(text, flames)
 
-        await scheduler.delay(60 * 2)
-        controls.group("Bottom fire").value("Fire maximum intensity").value = 380
-
-        await scheduler.delay(60 * 2)
-        controls.group("Bottom fire").value("Fire maximum intensity").value = 600
-
-        await scheduler.delay(60 * 2)
-        controls.group("Bottom fire").value("Fire maximum intensity").value = 700
+        // await scheduler.delay(60 * 2)
+        // controls.group("Bottom fire").value("Fire maximum intensity").value = 380
+        //
+        // await scheduler.delay(60 * 2)
+        // controls.group("Bottom fire").value("Fire maximum intensity").value = 600
+        //
+        // await scheduler.delay(60 * 2)
+        // controls.group("Bottom fire").value("Fire maximum intensity").value = 700
 
 
     }
