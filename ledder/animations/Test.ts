83bc2ba446a52318eac249895d69f8a35ee07112
Edwin Eefting <edwin@datux.nl>
2023-02-07 21:05:37 +0100

    added image support

--- ledder/animations/Test.ts
+++ ledder/animations/Test.ts
@@ -4,8 +4,6 @@ import ControlGroup from "../ControlGroup.js"
 import PixelBox from "../PixelBox.js"
 import DrawImage from "../draw/DrawImage.js"
 import sharp from "sharp"
-import {data} from "dom7"
-import {info} from "../../src/js/web/util.js"
 import FxColorCycle from "../fx/FxColorCycle.js"
 import FxColorPattern from "../fx/FxColorPattern.js"
 
@@ -15,12 +13,12 @@ export default class Test extends Animator {
     async run(box: PixelBox, scheduler: Scheduler, controls: ControlGroup) {
         // const {data, info} = await sharp('/home/psy/Downloads/hackerhotel1.png').trim().resize({width: 75, height:16}).raw().toBuffer({resolveWithObject: true})
         // const {data, info} = await sharp('/home/psy/Downloads/pixil-frame-0.png').trim().resize({width: 75, height:16, fit:'fill'}).raw().toBuffer({resolveWithObject: true})
-        const image = await sharp('/home/psy/Downloads/hackerhotel1.png')
-            .trim()
-            // .resize({width: 80, height:32, fit:'contain'})
-            .negate({alpha: false})
-            .resize({width: 80, height:32, fit:'inside', kernel: "lanczos3" })
-            // .threshold(100)
+        // const image = await sharp('/home/psy/Downloads/hackerhotel1.png')
+        const image = await sharp('images/hackerhotel.png')
+            // .threshold(255)
+            // .trim()
+            // .negate({alpha: false})
+            // .resize({width: 80, height:32, fit:'inside', kernel: "lanczos3" })
 
 
         box.add(await new DrawImage().addImage(0,0,image))
