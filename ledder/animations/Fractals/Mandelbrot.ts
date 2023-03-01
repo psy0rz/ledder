8163c61dd92f09adf3917b9c7875c2dcfbf23ffb
Rein Velt <rein@velt.org>
2023-03-01 08:52:28 +0100

    cleanup

--- ledder/animations/Fractals/Mandelbrot.ts
+++ ledder/animations/Fractals/Mandelbrot.ts
@@ -9,14 +9,14 @@ import Pixel from "../../Pixel.js"
 import Color from "../../Color.js";
 import Animator from "../../Animator.js"
 import {inspect} from "util"
-const MAX_ITERATION = 500
+const MAX_ITERATION = 2000
 
 
 export default class <Mandelbrot> extends Animator {
     static category = "Fractals"
     static title = "Mandelbrot"
     static description = "Base on the 1993 Firedemo"
-    
+    max_iterations=20
    
 
     mandelbrot(c) {
@@ -32,42 +32,21 @@ export default class <Mandelbrot> extends Animator {
             };
             d = Math.sqrt(Math.pow(z.x, 2) + Math.pow(z.y, 2));
             n += 1;
-        } while (d <= 2 && n < MAX_ITERATION);
+        } while (d <= 2 && n < this.max_iterations);
         return [n, d <= 2];
     }
 
-    makeRGB(r,g,b,a)
-    {
-        let c=new Color();
-        c.r=r;
-        c.g=g;
-        c.b=b;
-        c.a=a;
-        return c;
-    }
-
-    getPalette(palettesize=10){
-        const segment=Math.round(palettesize / 3)
-        let colors = []
-        let c
-        for (let k = 0; k < palettesize; k++) {
-            c=new Color();
-            c.r=Math.round((Math.sin((k/8)-0)+1)*128);
-            c.g=Math.round((Math.sin((k/8)-180)+1)*128);
-            c.b=Math.round((Math.cos((k/8)-0)+1)*128);
-            colors.push(c);
-        }
-        console.log(colors);
-        return colors
-    }
+   
 
     async run(box: PixelBox, scheduler: Scheduler, controls: ControlGroup) {
 
         const fractalColorCycle = controls.switch("Palette Cycle", false, true);
         const fractalZoom = controls.switch("Zoomer", true, true);
-        const fractalRealRange = controls.range("REAL", -2,1, -2,1,0.1)
-        const fractalImaginaryRange = controls.range("IMAGINARY", -1, 1, -1, 1,0.1)
-        const colors = patternSelect(controls, 'Color Palette', 'Bertrik fire')
+        //const fractalRealRange = controls.range("REAL", -2,1, -2,1,0.1)   //unused.. fixed for now
+        //const fractalImaginaryRange = controls.range("IMAGINARY", -1, 1, -1, 1,0.1)  //unused..fixed for now
+        const fractalIterations = controls.value("Fractal iterations", 128, 1, 1000, 1)
+       
+        const colors = patternSelect(controls, 'Color Palette', 'Brainsmoke fire')
         const fractalintervalControl = controls.value("Fractal interval", 1, 1, 10, 0.1)
         let x=0;
         let cx=0;
@@ -83,21 +62,18 @@ export default class <Mandelbrot> extends Animator {
             let pc=0
             let iterations=0; 
             q=q+1
-            if (fractalColorCycle.enabled)
-            {
-               pc=q
-            }
-            else
-            { 
-                pc=0
-            }
 
+            //read settings
+            this.max_iterations=fractalIterations.value     
+            if (fractalColorCycle.enabled){ pc=q }
             if (fractalZoom.enabled)
             {
                 REAL_SET = { start: -2+Math.sin(q/100)+0, end: 1-Math.cos(q/100)-0 }
                 IMAGINARY_SET = { start: -1+Math.sin(q/100)+0, end: 1-Math.cos(q/100)-0 }
             }
            
+            //calculate all visible pixel positions
+            //box.clear();
             for (let i = 0; i < box.width(); i++) {
                 for (let j = 0; j < box.height(); j++) {
                     let complex = {
@@ -107,8 +83,6 @@ export default class <Mandelbrot> extends Animator {
                     const [m, isMandelbrotSet] = this.mandelbrot(complex);
                     if (m!=false)
                     {
-                       
-                       
                         let color= colors[Math.round((+m+pc) % (colors.length-1))];
                         let pixel = new Pixel(i,j,color);
                         box.add(pixel);
