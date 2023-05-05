6421bc4a385bec68693561163b2a02a9fdfdb27b
Rein Velt <rein@velt.org>
2023-05-05 23:22:02 +0200

    clock fix

--- ledder/fonts.ts
+++ ledder/fonts.ts
@@ -8,19 +8,19 @@ import ControlGroup from "./ControlGroup.js";
 
 
 export let fonts = {
-    'Atari regular': new Font('Atari', 'fonts/EightBit Atari-Regular.ttf', 0, 8, 0),
-    'C64 mono': new Font('C64 monospaced', 'fonts/C64_Pro_Mono-STYLE.otf', 0, 8, 0),
-    'C64': new Font('C64', 'fonts/C64_Pro-STYLE.otf', 0, 8, 0),
-    'IBM bios': new Font('IBM bios', 'fonts/PxPlus_IBM_BIOS.ttf', 0, 8, 0),
-    'MSX': new Font('MSX', 'fonts/MSX-Screen0.ttf', 0, 8, 0),
-    'ZX Sierra Quest': new Font('ZX Sierra Quest', 'fonts/ZX Sierra Quest.ttf', 0, 8, -1),
-    'Anarchist': new Font('Anarchist', 'fonts/Anarchist.ttf', 0, 8, 1),
-    'Skid Row': new Font('Skid Row', 'fonts/Skid Row.ttf', 0, 8, 0),
-    'Quasar': new Font('Quasar', 'fonts/Quasar.ttf', 0, 8, 0),
-    'Computer': new Font('Computer', 'fonts/Computer.ttf', 0, 8, 0),
-    'Picopixel': new Font('Picopixel', 'fonts/Picopixel.ttf', 0, 7, -1),
-    'ORG v01': new Font('Org v01', 'fonts/ORG_V01_.TTF', 0, 8, -2),
-    'Tiny 3x3': new Font('Tiny 3x3', 'fonts/tiny3x3a.ttf', 0, 4, 0),
+    'Atari regular': new Font('Atari', 'fonts/EightBit Atari-Regular.ttf', 8, 8, 0),
+    'C64 mono': new Font('C64 monospaced', 'fonts/C64_Pro_Mono-STYLE.otf', 8, 8, 0),
+    'C64': new Font('C64', 'fonts/C64_Pro-STYLE.otf', 7, 8, 0),
+    'IBM bios': new Font('IBM bios', 'fonts/PxPlus_IBM_BIOS.ttf', 8, 8, 0),
+    'MSX': new Font('MSX', 'fonts/MSX-Screen0.ttf', 8, 8, 0),
+    'ZX Sierra Quest': new Font('ZX Sierra Quest', 'fonts/ZX Sierra Quest.ttf', 8, 8, -1),
+    'Anarchist': new Font('Anarchist', 'fonts/Anarchist.ttf', 8, 8, 1),
+    'Skid Row': new Font('Skid Row', 'fonts/Skid Row.ttf', 8, 8, 0),
+    'Quasar': new Font('Quasar', 'fonts/Quasar.ttf', 8, 8, 0),
+    'Computer': new Font('Computer', 'fonts/Computer.ttf', 7, 8, 0),
+    'Picopixel': new Font('Picopixel', 'fonts/Picopixel.ttf', 7, 7, -1),
+    'ORG v01': new Font('Org v01', 'fonts/ORG_V01_.TTF', 6, 8, -2),
+    'Tiny 3x3': new Font('Tiny 3x3', 'fonts/tiny3x3a.ttf', 4, 4, 0),
     // 'Tom thumb': new Font('Tom thumb', 'fonts/tom-thumb.bdf', 0, 6, 0),
 }
 
