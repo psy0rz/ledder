780f010d57f652cee6d23562693424bee17d0733
Edwin Eefting <edwin@datux.nl>
2023-07-07 10:52:23 +0200

    rammmmsteinnnn

---
+++ ledder/animations/Logos/Rammstein.ts
@@ -0,0 +1,70 @@
+import PixelBox from "../../PixelBox.js"
+import sharp from "sharp"
+import drawImage from "../../draw/DrawImage.js"
+import Scheduler from "../../Scheduler.js"
+import ControlGroup from "../../ControlGroup.js"
+import Animator from "../../Animator.js"
+import PixelList from "../../PixelList.js"
+import Color from "../../Color.js"
+import FxAutoTrace from "../../fx/FxAutoTrace.js"
+import FxColorPattern from "../../fx/FxColorPattern.js"
+import FxMove from "../../fx/FxMove.js"
+import FxRotate from "../../fx/FxRotate.js"
+
+
+export default class Hackerhotel extends Animator {
+
+    async run(box: PixelBox, scheduler: Scheduler, controls: ControlGroup) {
+
+        //load image and determine colors
+        const image = await sharp('images/rammstein.png')
+        const imageLetterColor = new Color(255, 216, 0)
+
+        const logo = await drawImage(0, 0, image)
+        logo.setColor(imageLetterColor)
+        box.add(logo)
+
+        //get letters:
+        // const letterColorControl=controls.color('Text color', imageLetterColor.r, imageLetterColor.g, imageLetterColor.b)
+        // const letters = logo.filterColor(imageLetterColor)
+        // letters.setColor(letterColorControl)
+        // box.add(letters)
+
+        //find start of each trace and filter it out
+        // const traces=new PixelList()
+        // box.add(traces)
+        //
+        // logo.forEachPixel((p) => {
+        //     if (imageTraceStartColor.equal(p.color)) {
+        //         const trace = logo.filterCluster(p)
+        //         traces.add(trace)
+        //     }
+        // })
+        //
+        // //make trace color configurable
+        // const traceColorControl=controls.color('Trace color', imageTraceColor.r, imageTraceColor.g, imageTraceColor.b)
+        // box.replaceColor(imageTraceColor, traceColorControl)
+        //
+        // const patternGroup=controls.group('Color pattern')
+        // if (patternGroup.switch('Color pattern fx', false).enabled)
+        //     new FxColorPattern(scheduler, patternGroup
+        //     ).run(letters)
+
+        // new FxMove(scheduler,controls).run(box)
+
+        new FxRotate(scheduler, controls).run(box, {
+            xMin: box.bbox().xMin,
+            xMax: box.bbox().xMax+64,
+            yMin: box.bbox().yMin,
+            yMax: box.bbox().yMax,
+
+        })
+
+
+        //start trace-effect on all the traces we've found
+        // const autoTraceFx=new FxAutoTrace(scheduler,controls)
+        // await autoTraceFx.run(traces, box)
+
+
+    }
+}
