c6d368c744d6a24ce2efde097d35bb621ff258fb
Rein Velt <rein@velt.org>
2023-09-30 23:58:44 +0200

    DrawLine

--- ledder/draw/DrawLine.ts
+++ ledder/draw/DrawLine.ts
@@ -1,16 +1,27 @@
-// import Draw from "../Draw.js";
-// import Pixel from "../Pixel.js";
-// import ColorInterface from "../ColorInterface.js";
-//
-// //draw a simple filled box. x,y are top left coordinates
-// export default class DrawBox extends Draw {
-//     constructor(x: number, y: number, width: number, height: number, color: ColorInterface) {
-//         super();
-//
-//         for (let thisX = x; thisX < x + width; thisX++)
-//             for (let thisY = y; thisY < y + height; thisY++)
-//                 this.add(new Pixel(thisX, thisY, color))
-//     }
-// }
-//
-//
+ import Draw from "../Draw.js";
+ import Pixel from "../Pixel.js";
+ import PixelList from "../PixelList.js";
+ import ColorInterface from "../ColorInterface.js";
+
+ //draw a simple line .
+ export default class DrawLine extends Draw {
+     constructor(x1: number, y1: number, x2: number, y2: number, color: ColorInterface) {
+        let line=new PixelList()
+         super();
+         let xDiff=x2-x1
+         let yDiff=y2-y1
+         let xDelta=Math.sign(xDiff)
+         let yDelta=Math.sign(yDiff)
+         let yDeltaPerX=yDiff/xDiff
+         let y=y1
+         for (let x = x1; x < x2 ; x+=xDelta)
+         {
+            y=y+yDeltaPerX
+            line.add(new Pixel(x,y,color))
+         }
+         this.add(line)
+
+     }
+ }
+
+
