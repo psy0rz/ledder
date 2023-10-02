e58fafac7356e0f6e3949e85694a5533d7e4586b
Rein Velt <rein@velt.org>
2023-10-03 01:58:47 +0200

    Drawline fix

--- ledder/draw/DrawLine.ts
+++ ledder/draw/DrawLine.ts
@@ -7,35 +7,33 @@
 
  //draw a simple line .
  export default class DrawLine extends Draw {
-     constructor(x1: number, y1: number, x2: number, y2: number, color1: ColorInterface,color2: ColorInterface,antialias:boolean=true) {
+     constructor(x1: number, y1: number, x2: number, y2: number, color1: ColorInterface,color2: ColorInterface) {
        
          super();
+         let pixel1=new Pixel(x1,y1,color1)
+         let pixel2=new Pixel(x2,y2,color2)
+        
+         
          let line=new PixelList()
-         line.add(new Pixel(0,0,new Color(0,0,0,0)))
-         let xDiff=(x2-x1)
-         let xDiffAbs=Math.abs(xDiff)
-         let yDiff=y2-y1
-         let ratio=1
-         if (xDiff!=0) { ratio=yDiff/(xDiffAbs) }
-         let xDelta=Math.sign(xDiff)
-         let yDeltaPerX=yDiff/xDiff
-         let y=y1
-         for (let x = 0; x < xDiffAbs ; x=x+Math.abs(xDelta))
+         let xDiff=(pixel2.x-pixel1.x)
+         let yDiff=(pixel2.y-pixel1.y)
+         let distance=Math.sqrt(Math.pow(xDiff,2)+Math.pow(yDiff,2))
+ 
+
+         if (distance>0)
          {
-            y=y+yDeltaPerX
-            let weight=0;
-            if (xDiff!=0) { weight=x/xDiff } else {weight=1}
-            for (let yy=0;yy<Math.abs(ratio);yy++)
+            let x=pixel1.x
+            let y=pixel1.y
+            for (let i=0; i<=distance; i++)
             {
-              //console.log(x,y,weight)
-              line.add(new Pixel(x1+x,y+yy,this.interpolateColor(color1,color2,Math.abs(weight))))
-              if (antialias)
-              {
-                line.add(new Pixel(x1+x+0.5,y+yy+0.5,this.interpolateColor(color1,color2,Math.abs(weight/2))))
-              }
+                let weight=i/distance
+               
+                line.add(new Pixel(x,y,this.interpolateColor(color1,color2,Math.abs(weight))))
+                x=x+xDiff/distance
+                y=y+yDiff/distance
             }
+            this.add(line)
          }
-         this.add(line)
 
      }
 
@@ -49,6 +47,8 @@
         let a=Math.min(1.0,(c1.a*(1-weight)) + (c2.a*weight))
         return new Color(r,g,b,a)
      }
+
+
 
  }
 
