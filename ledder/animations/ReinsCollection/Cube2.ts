ed656a8d8122d56bd962d6df2e36bec94fc6ddff
Rein Velt <rein@velt.org>
2023-10-30 01:40:55 +0100

    cube2

--- ledder/animations/ReinsCollection/Cube2.ts
+++ ledder/animations/ReinsCollection/Cube2.ts
@@ -46,9 +46,11 @@ export class Vec3 extends Matrix {
 
 export class Vec3Color extends Matrix {
    
-    constructor(x:number,y:number,z:number,c:Color)
+    color:Color
+    constructor(x:number,y:number,z:number,color:Color)
     {
         super({x:x,y:y,z:z})
+        this.color=color
     }
 
    
@@ -138,7 +140,9 @@ export class Object3d {
         //transform points and put all transformed points into an new array (dont mess with the originals)
         for (let p=0;p<this.points.length;p++)
         {
-            pointBuffer.push(this.points[p].rotateX(this.transformation.rotate.x).rotateY(this.transformation.rotate.y).rotateZ(this.transformation.rotate.z))
+            let newpoint=this.points[p].rotateX(this.transformation.rotate.x).rotateY(this.transformation.rotate.y).rotateZ(this.transformation.rotate.z)
+            newpoint.color=this.points[p].color
+            pointBuffer.push(newpoint)
         }
 
         let depthLimits=this.getZProjectionlimits(pointBuffer)
@@ -152,6 +156,8 @@ export class Object3d {
             let x=pointBuffer[p].x+this.transformation.translate.x
             let y=pointBuffer[p].y+this.transformation.translate.y
             let z=pointBuffer[p].z+this.transformation.translate.z
+            let c=this.color
+            if (this.points[p].color) { c=this.points[p].color } else {}
 
             //depth correction (perspective)
             let xDiff=x-centerX
@@ -162,12 +168,12 @@ export class Object3d {
             
 
             //color (more distance is less alpha)
-            let c=this.color.copy()
-            c.a=1-Math.max(0.1,(pointBuffer[p].z)*depthLimits.alphaPerZ)
-            perspectiveBuffer.push({x:x,y:y,z:z,a:c.a})
+            let depthcolor=c.copy()
+            depthcolor.a=1-Math.max(0.1,(pointBuffer[p].z)*depthLimits.alphaPerZ)
+            perspectiveBuffer.push({x:x,y:y,z:z,a:depthcolor.a})
    
             //draw point
-            pl.add(new Pixel(x,y,c))
+            pl.add(new Pixel(x,y,depthcolor))
         }
 
         
@@ -181,13 +187,13 @@ export class Object3d {
                 let x1=perspectiveBuffer[p1].x
                 let y1=perspectiveBuffer[p1].y
                
-                let c1=this.color.copy()
+                let c1=pointBuffer[p1].color.copy()
                 c1.a=perspectiveBuffer[p1].a
 
                 let x2=perspectiveBuffer[p2].x
                 let y2=perspectiveBuffer[p2].y
               
-                let c2=this.color.copy()
+                let c2=pointBuffer[p2].color.copy()
                 c2.a=perspectiveBuffer[p2].a
 
                 pl.add(new DrawLine(x1,y1,x2,y2,c1,c2))
@@ -251,17 +257,17 @@ class Cube3d extends Object3d {
         let halfHeight=height/2
         let halfDepth=depth/2
 
-        this.points.push(new Vec3(-1*halfWidth,1*halfHeight,-1*halfDepth)) //top-left-far
-        this.points.push(new Vec3(1*halfWidth,1*halfHeight,-1*halfDepth)) //top-right-far
-        this.points.push(new Vec3(-1*halfWidth,1*halfHeight,1*halfDepth)) //top-left-close
-        this.points.push(new Vec3(1*halfWidth,1*halfHeight,1*halfDepth)) //top-right-close
+        this.points.push(new Vec3Color(-1*halfWidth,1*halfHeight,-1*halfDepth,color)) //top-left-far
+        this.points.push(new Vec3Color(1*halfWidth,1*halfHeight,-1*halfDepth,color)) //top-right-far
+        this.points.push(new Vec3Color(-1*halfWidth,1*halfHeight,1*halfDepth,color)) //top-left-close
+        this.points.push(new Vec3Color(1*halfWidth,1*halfHeight,1*halfDepth,color)) //top-right-close
 
-        this.points.push(new Vec3(-1*halfWidth,-1*halfHeight,-1*halfDepth)) //bot-left-far
-        this.points.push(new Vec3(1*halfWidth,-1*halfHeight,-1*halfDepth)) //bot-right-far
-        this.points.push(new Vec3(-1*halfWidth,-1*halfHeight,1*halfDepth)) //bot-left-close
-        this.points.push(new Vec3(1*halfWidth,-1*halfHeight,1*halfDepth)) //bot-right-close
+        this.points.push(new Vec3Color(-1*halfWidth,-1*halfHeight,-1*halfDepth,color)) //bot-left-far
+        this.points.push(new Vec3Color(1*halfWidth,-1*halfHeight,-1*halfDepth,color)) //bot-right-far
+        this.points.push(new Vec3Color(-1*halfWidth,-1*halfHeight,1*halfDepth,color)) //bot-left-close
+        this.points.push(new Vec3Color(1*halfWidth,-1*halfHeight,1*halfDepth,color)) //bot-right-close
 
-        this.points.push(new Vec3(0,0,0)) //center
+        this.points.push(new Vec3Color(0,0,0,color)) //center
 
 
         this.lines.push(new CoordinateLine(0,1))
@@ -325,16 +331,16 @@ class Sphere3d extends Object3d {
         let depth=radius*2
         super(x,y,z,width,height,depth,color)
         let halfRadius=radius/2
-        let segments=radius
+        let segments=4
         this.points.push(new Vec3(0,0,0)) //center
       
             for (let p=0;p<360+(360/segments);p=p+(360/segments))
             {
                 let px=Math.sin(p * (Math.PI / 180) ) * halfRadius
                 let py=Math.cos(p * (Math.PI / 180) ) * halfRadius
-                this.points.push(new Vec3(px,py,0)) //top-left-far
-                this.points.push(new Vec3(0,py,px)) //top-left-far
-                this.points.push(new Vec3(px,0,py)) //top-left-far
+                this.points.push(new Vec3Color(px,py,0,color)) //top-left-far
+                this.points.push(new Vec3Color(0,py,px,color)) //top-left-far
+                this.points.push(new Vec3Color(px,0,py,color)) //top-left-far
                
                 let ll=this.points.length
                 if (ll>7)
@@ -346,6 +352,57 @@ class Sphere3d extends Object3d {
             }
     }
 }
+
+class Spiral3d extends Object3d {
+    constructor(x:number,y:number,z:number, radius:number,length:number, color:Color)
+    {
+        
+        let width=radius*2
+        let height=radius*2
+        let depth=length
+        super(x,y,z,width,height,depth,color)
+        let halfRadius=radius/2
+        let segments=6
+        this.points.push(new Vec3(0,0,0)) //center
+      
+            for (let l=0;l<length;l=l+(1/segments))
+            {
+               
+                let px=Math.round(Math.sin((l*segments) * (Math.PI / 180) ) * halfRadius)
+                let py=Math.round(Math.cos((l*segments) * (Math.PI / 180) ) * halfRadius)
+                this.points.push(new Vec3(px,py,l)) //top-left-far
+                
+                if (l>0)
+                {
+                    this.lines.push(new CoordinateLine(this.points.length-1,this.points.length-2))
+                   
+                }
+            }
+    }
+}
+
+class Random3d extends Object3d{
+    constructor(x:number,y:number,z:number, width,height,depth,particlecount, color:Color)
+    {
+        
+       
+        super(x,y,z,width,height,depth,color)
+        this.points.push(new Vec3Color(0,0,0,new Color(255,255,255,1))) //top-left-far
+      
+        for (let p=0;p<particlecount;p++)
+        {
+            
+            let px=random(0,width)-(width/2)
+            let py=random(0,height)-(height/2)
+            let pz=random(0,depth)-(depth/2)
+            this.points.push(new Vec3Color(px,py,pz,new Color(0,0,random(0,255),Math.random()))) //top-left-far
+            //this.lines.push(new CoordinateLine(this.points.length-1,0))
+                
+           
+        }
+    }
+}
+
 
 
 
@@ -361,30 +418,39 @@ export default class Project3 extends Animator {
         const gameIntervalControl = gameControls.value("Clock interval", 1, 1, 10, 0.1, true)
         const gameRotateControl = gameControls.value("Rotation per clock interval", 1, -45, 45, 0.1, true)
         const gamePerspectiveControl = gameControls.value("Perspective factor", 0.97, 0.9, 1.0, 0.01, true)
+        const gameBoxsizeControl = gameControls.value("Boxsize", box.height(), 1, 255, 1, true)
+        const gameStarsControl = gameControls.value("Stars", 10, 0, 1000, 1, true)
         const gameWireframeControl=gameControls.switch("Draw wireframelines",true,true)
 
-        let  controlSettings={rotation:gameRotateControl.value,wireframe:gameWireframeControl.enabled,perspective:gamePerspectiveControl.value}
+        let  controlSettings={rotation:gameRotateControl.value,wireframe:gameWireframeControl.enabled,perspective:gamePerspectiveControl.value,stars:gameStarsControl.value}
         let pl=new PixelList();
         box.add(pl)
 
         let scene3d=new scene3D()
-        let cubeC=new Cube3d(box.width()/2,box.height()/2,20, box.height(),box.height(),box.height(),new Color(0,0,128,1.0))
-        let cubeL=new Pyramid3d(0,box.height()/2,20, box.height(),box.height(),box.height(),new Color(128,0,0,0.6))
-        let cubeR=new Sphere3d(box.width(),box.height()/2,20, box.height(),new Color(0,128,0,0.6))
+        let cubeC=new Cube3d(box.width()/2,box.height()/2,20, gameBoxsizeControl.value,gameBoxsizeControl.value,gameBoxsizeControl.value,new Color(128,128,0,1.0))
+        //let cubeL=new Pyramid3d(0,box.height()/2,20, box.height(),box.height(),box.height(),new Color(128,0,0,0.6))
+        //let sphere=new Sphere3d(box.width(),box.height()/2,20, box.height(),new Color(0,128,0,0.6))
+        //let spiral=new Spiral3d(box.width()/2,box.height()/2,20, box.height(),box.height(),new Color(0,128,0,0.6))
+        let stars=new Random3d(box.width()/2,box.height()/2,20, gameBoxsizeControl.value,gameBoxsizeControl.value,gameBoxsizeControl.value,gameStarsControl.value,new Color(0,0,128,0.5))
+        scene3d.objects.push(stars)
         
         scene3d.objects.push(cubeC)
-        scene3d.objects.push(cubeL)
-        scene3d.objects.push(cubeR)
+        //scene3d.objects.push(spiral)
+        //scene3d.objects.push(cubeL)
+        //scene3d.objects.push(cubeR)
+      
+
         
         let rotate=0
     
         scheduler.intervalControlled(gameIntervalControl, (frameNr) => {
             pl.clear()
             rotate=rotate+gameRotateControl.value
-            for (let p=0;p<scene3d.objects.length;p++)
-            {
-                scene3d.objects[p].setRotation(rotate,rotate,0)
-            }
+            //scene3d.objects[1].setRotation(0,0,rotate*4)
+            scene3d.objects[1].setRotation(rotate,rotate,0)
+            scene3d.objects[0].setRotation(rotate,rotate,0)
+           
+  
             pl.add(scene3d.render(box, controlSettings))
            
        
