b4339bfd304aaa33d6a171fdd2870a7dc61e3258
Rein Velt <rein@velt.org>
2023-11-16 09:50:29 +0100

    photon marquee and flames

--- ledder/animations/ReinsCollection/Photon.ts
+++ ledder/animations/ReinsCollection/Photon.ts
@@ -8,6 +8,8 @@ import DrawLine from "../../draw/DrawLine.js"
 import Pixel from "../../Pixel.js"
 import Color from "../../Color.js"
 import { patternSelect } from "../../ColorPatterns.js"
+import Marquee from "../Text/Marquee.js"
+import Fire from "../Fires/Fire.js"
 
 class PhotonPixel 
 {
@@ -70,7 +72,7 @@ class PhotonStack
             red=Math.round(rSum/rMass)
             green=Math.round(gSum/gMass)
             blue=Math.round(bSum/bMass)
-            alpha=aSum/aMass
+            alpha=Math.min(0.8,aSum/aMass)
             this.stack=[]
             this.stack.push(new PhotonPixel(new Pixel(this.x,this.y,new Color(red,green,blue,alpha))))
         }
@@ -104,6 +106,8 @@ class Photonmatrix
     photonStack: PhotonStack[]
     palette:Color[]
     magic:number
+    magic2:number
+
     radius:number
     
 
@@ -114,6 +118,7 @@ class Photonmatrix
         this.photonStack=[]
         this.palette=[]
         this.magic=0
+        this.magic2=0
         this.radius=0
         for (let y=0;y<height;y++)
         {
@@ -257,6 +262,98 @@ class Photonmatrix
         
     }
 
+    julia(c,px:number,py:number,width:number,height:number,zoom:number)
+    {
+        //calculate the initial real and imaginary part of z, based on the pixel location and zoom and position values
+         let newRe = 1.5 * (px - width / 2) / (0.5 * zoom * width) ;
+         let newIm = (py - height / 2) / (0.5 * zoom * height);
+         let oldRe=0;
+         let oldIm=0
+         //i will represent the number of iterations
+         let n=0
+         //start the iteration process
+         do
+         {
+             //remember value of previous iteration
+             oldRe = newRe;
+             oldIm = newIm;
+             //the actual iteration, the real and imaginary part are calculated
+             newRe = oldRe * oldRe - oldIm * oldIm + c.cx
+             newIm = 2 * oldRe * oldIm + c.cy
+             //if the point is outside the circle with radius 2: stop
+             if((newRe * newRe + newIm * newIm) > 4) break;
+             n++
+         } while (n<500)
+         return n
+    }
+
+    addJulia(colorpalette,colorindex,colorshift)
+    {
+        this.magic=Math.max(2,this.magic)
+        let hotspotsel=this.magic2
+        this.magic=Math.max(1,this.magic*1.001)
+        let hotspots=[
+            { cx:-0.74800888462543, cy:0.058641210013121,  maxz:2.80},
+            { cx:0.31997989023329, cy:0.037739114652372,   maxz:8},
+            { cx:0.30532223412791, cy:0.028030387481731,   maxz:8.05},
+            { cx:-1.4183762254903, cy:-0.00058322469430364, maxz:9}
+        ]
+        let c=hotspots[this.magic2]
+        let zoom=this.magic
+        if (zoom>Math.pow(10,c.maxz-1))
+        {
+            zoom=1
+            this.magic=1
+            this.magic2++
+            if (this.magic2>hotspots.length-1) {  this.magic2=0}
+        }
+       
+        for (let x=0;x<this.width;x++){
+            for (let y=0;y<this.height;y++){
+                let pixelcolor:number=this.julia(c,x,y,this.width,this.height,zoom)
+                let color:Color=colorpalette[pixelcolor%colorpalette.length]
+                if (pixelcolor>0)
+                {
+                    let c=color.copy()
+                    c.a=0.7
+                    this.addPixel(new Pixel(x,y,c))
+                }
+            }
+        }
+    }
+
+    addStripes(colorPalette, colorindex, colorshift)
+    {
+        
+            this.magic=this.magic+Math.random()*8
+            this.magic=this.magic%360
+            let cx=(this.width/2)
+            let cy=(this.height/2)
+            let rx=cx+(Math.sin(this.magic/60)*(this.width/2))
+            let ry=cy+(Math.cos(this.magic/60)*(this.width/2))
+            let distance=Math.sqrt(Math.pow(cx-rx,2)+(Math.pow(cy-ry,2)))
+            let xd=(rx-cx)/distance
+            let yd=(ry-cy)/distance
+
+            let x=cx
+            let y=cy
+                
+            for (let ll=0;ll<distance;ll++)
+            {
+                colorindex=colorindex+colorshift
+                colorindex=colorindex%colorPalette.length
+                let color=colorPalette[(colorindex)%colorPalette.length]
+                let ccolor=color.copy()
+                ccolor.a= Math.max(0,Math.min(1,ccolor.a/((ll+1)/colorPalette.length)))
+            
+                x=(x+xd)
+                y=(y+yd)
+                this.addPixel(new Pixel(Math.round(x),Math.round(y),ccolor))
+            }
+            
+        
+    }
+
     shift(pixelcount)
     {
 
@@ -287,20 +384,45 @@ export default class Photonmatrixtest extends Animator {
     static description = "Photon test"
 
     async run(box: PixelBox, scheduler: Scheduler, controls: ControlGroup) {
-        const animatorControl             = controls.group("Photon",true)
-        const colorshiftControl             = animatorControl.value("Color shift", 0, 0 32, 1)
-        const intervalControl             = animatorControl.value("Animation interval", 1, 1, 10, 1)
         let choices=[]
         choices.push({id:1, name:"Photon"})
         choices.push({id:2, name:"SinCos"})
         choices.push({id:3, name:"XY"})
         choices.push({id:4, name:"Clouds"})
-        const patternControl              =animatorControl.select("Pattern","Photon",choices,true)
-        const colorPaletteControl   = patternSelect(animatorControl, 'Color Palette', 'DimmedReinbow')
+        choices.push({id:5, name:"Stripes"})
+        choices.push({id:6, name:"Julia"})
+
+        const animatorControl               = controls.group("Photon",true)
+        const colorshiftControl             = animatorControl.value("Color shift", 0, 0 ,32, 1)
+        const intervalControl               = animatorControl.value("Animation interval", 1, 1, 10, 1)
+       
+        const patternControl                = animatorControl.select("Algoritm","Photon",choices,true)
+        const colorPaletteControl           = patternSelect(animatorControl, 'Color Palette', 'DimmedReinbow')
+        const fireEnabledControl            = animatorControl.switch("Fire enable",false,true)
+        const marqueeEnabledControl          = animatorControl.switch("Marquee enable",false,true)
+        const fireControl                   = animatorControl.group("Fire")
+        const marqueeControl                = animatorControl.group("Marquee")
+       
+       
+       
+       
         let photon=new Photonmatrix(box.width(),box.height())
+
+        if (fireEnabledControl.enabled)
+        {
+            let fire=new Fire()
+            fire.run(box,scheduler,fireControl)
+        }
+        
         let pixellist=new PixelList()
         box.add(pixellist)
 
+        
+        if (marqueeEnabledControl.enabled)
+        {
+            let marquee=new Marquee()
+            marquee.run(box,scheduler,marqueeControl)
+        }
       
         scheduler.intervalControlled(intervalControl, (frameNr) => {
             pixellist.clear()
@@ -317,6 +439,10 @@ export default class Photonmatrixtest extends Animator {
                 case "3":       photon.addRandomGrid(colorPaletteControl,frameNr%colorPaletteControl.length,colorshiftControl.value); break
                 case "Clouds":   
                 case "4":       photon.addClouds(colorPaletteControl,frameNr%colorPaletteControl.length,colorshiftControl.value); break
+                case "Stripes":   
+                case "5":       photon.addStripes(colorPaletteControl,frameNr%colorPaletteControl.length,colorshiftControl.value); break
+                case "Julia":   
+                case "6":       photon.addJulia(colorPaletteControl,frameNr%colorPaletteControl.length,colorshiftControl.value); break
             }
             photon.update()
             let newPixelData:PixelList=photon.render()
