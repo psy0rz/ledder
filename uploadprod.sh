9e343d75f7b31439e0f92c75e4595b7bebe29076
Edwin Eefting <edwin@datux.nl>
2024-10-04 19:48:18 +0200

    fix

--- uploadprod.sh
+++ uploadprod.sh
@@ -11,4 +11,4 @@ STUFF="images src www presets fonts ledder package.json"
 
 #rsync -avx $STUFF root@$IP:ledder ||exit 1
 #ssh root@$IP 'pkill node ;cd ledder && npm run production'
-find $STUFF| entr -r -s "rsync -avx $STUFF root@$IP:ledder && ssh root@$IP 'pkill loop; pkill node; cd /root/ledder; NODE_ENV=production node ledder/server/server.js'"
+find $STUFF| entr -r -s "rsync -avx --exclude index.json $STUFF root@$IP:ledder && ssh root@$IP 'pkill loop; pkill node; cd /root/ledder; NODE_ENV=production node ledder/server/server.js'"
