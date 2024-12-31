22205a7c877e61e9f6e036d1bdf6c31e13a76c02
Edwin Eefting <edwin@datux.nl>
2024-12-31 15:56:36 +0100

    binary pixelflut

--- ledder/DisplayPixelflutBinary.ts
+++ ledder/DisplayPixelflutBinary.ts
@@ -2,24 +2,17 @@ import Display from "./Display.js"
 import ColorInterface from "./ColorInterface.js"
 import * as net from "node:net"
 import Color from "./Color.js"
-import {send} from "vite"
+import dgram from "dgram"
 
 const encoder = new TextEncoder()
-
-
 /*
-Text-mode pixelflooder.
-
-We pre-generate one fixed send buffer and flood that in a loop.
-Then the driver will manipulate the colors in this buffer with the new data.
-
-NOTE: it sent 125 FPS @118 MB/s with my laptop, only using 33%CPU.
-
-
+ Binary mode pixel flooder, in UDP mode
+https://github.com/JanKlopper/pixelvloed/blob/master/protocol.md
 
  */
-export default class DisplayPixelflut extends Display {
-    client: net.Socket
+export default class DisplayPixelflutBinary extends Display {
+    client: dgram.Socket
+
     frameBuffer: Array<Array<Color>>
 
     sendBuffer: Uint8Array
@@ -36,11 +29,14 @@ export default class DisplayPixelflut extends Display {
 
     flood: boolean
 
+    host: string
+    port: number
+
     /*
       If flood is true, it will keep sending the current frame as fast as possible. Otherwise it will be at the fps of this driver (60fps, adjustable )
       gridsize and pixelsize determine how many display pixels each ledder pixel will be. If the grid is bigger than then pixels, you will get a nice led-like rendering.
      */
-    constructor(width, height, host, port, gridSize = 1, pixelSize = 1, flood = false, sendOffset=false) {
+    constructor(width, height, host, port, gridSize = 1, pixelSize = 1, flood = false) {
         super(width, height)
 
         this.statsBytesSend = 0
@@ -71,12 +67,10 @@ export default class DisplayPixelflut extends Display {
             this.sendBufferOffsets.push(Ys)
             for (let y = 0; y < this.height; y++) {
                 Ys.push([])
-
             }
         }
 
         //create static sendbuffer, in random order to smooth out vsync/hsync
-
         //we want to shuffle every pixel. create linair sequence of PX commands first:
         const sequence = []
         for (let x = 0; x < width; x++) {
@@ -88,7 +82,17 @@ export default class DisplayPixelflut extends Display {
                 for (let thisX = xScaled; thisX < xScaled + pixelSize; thisX++) {
                     for (let thisY = yScaled; thisY < yScaled + pixelSize; thisY++) {
 
-                        sequence.push([x, y, `PX ${thisX} ${thisY} ffffff\n`])
+                        //protocol 0
+                        const buffer = new ArrayBuffer(7)
+                        const view = new DataView(buffer)
+
+                        view.setUint16(0, thisX, true)
+                        view.setUint16(2, thisY, true)
+                        view.setUint8(4, 255) //r
+                        view.setUint8(5, 0)   //g
+                        view.setUint8(6, 0)   //b
+
+                        sequence.push([x, y, new Uint8Array(buffer)])
                     }
                 }
             }
@@ -97,45 +101,33 @@ export default class DisplayPixelflut extends Display {
         //shuffle it
         const shuffled = sequence.sort(() => Math.random() - 0.5)
 
-        //create the grand-string-of-shuffled-PX commands
-        let buff = ""
+        //create the grand-uint8array-of-shuffed-binaryof-shuffled-commands
+        let buff = []
+
+        //select mode 0, no alpha
+        buff.push(0)
+        buff.push(0)
         for (const xyPX of shuffled) {
 
-            const [x, y, px] = xyPX
+            const [x, y, cmd] = xyPX
 
-            buff = buff + px
-            this.sendBufferOffsets[x][y].push(buff.length - 7)
+            for (const b of cmd) {
+                buff.push(b)
+            }
+
+            this.sendBufferOffsets[x][y].push(buff.length - 3)
         }
 
         //convert to uint8array so we can manipulate it efficiently
-        this.sendBuffer = new Uint8Array(buff.length)
-        this.sendBuffer.set(encoder.encode(buff))
+        this.sendBuffer = new Uint8Array(buff)
 
-        this.client = new net.Socket()
-        this.client.setNoDelay(true)
+        this.client = dgram.createSocket('udp4')
 
-
-        this.client.on('connect', () => {
-            if (sendOffset)
-                this.client.write('OFFSET 5,5\n')
+        this.host = host
+        this.port = port
 
 
-            if (flood)
-                while (this.send()) {
-                }
-
-
-        })
-
-        this.client.on('drain', () => {
-
-            if (flood)
-                while (this.send()) {
-                }
-
-        })
-
-        this.client.connect(port, host)
+//stats
         setInterval(() => {
             if (this.statsBytesSend === 0)
                 console.log('PixelFlut: STALLED')
@@ -144,81 +136,48 @@ export default class DisplayPixelflut extends Display {
             this.statsBytesSend = 0
             this.statsFpsSend = 0
 
-            if (this.client.readyState == "closed") {
-                console.log("Pixelflut: Reconnecting")
-                this.client.connect(port, host)
-            }
 
         }, 1000)
 
-
-        this.client.on('error', (e) => {
-            console.error(`PixelFLut: ${e}`)
-        })
     }
 
-    updateOffsets() {
-        return
-        if (this.offsetX <= 0)
-            this.stepX = this.stepSize
-
-        if (this.offsetX > 3300)
-            this.stepX = -this.stepSize
-
-        if (this.offsetY <= 0)
-            this.stepY = this.stepSize
-
-        if (this.offsetY > 900)
-            this.stepY = -this.stepSize
-
-        this.offsetX = this.offsetX + this.stepX
-        this.offsetY = this.offsetY + this.stepY
-
-
-        this.client.write(`OFFSET ${this.offsetX},${this.offsetY}\n`)
-    }
 
     send() {
         this.statsFpsSend = this.statsFpsSend + 1
         this.statsBytesSend = this.statsBytesSend + this.sendBuffer.length
 
-        this.updateOffsets()
 
-        return this.client.write(this.sendBuffer)
 
+        this.client.send(this.sendBuffer, this.port, this.host, (err) => {
+            if (err) {
+                console.error('Error sending data:', err)
+            }
+        })
     }
 
     frame(displayTimeMicros: number) {
 
-        //dont waist resources when not connected
-        if (this.client.readyState !== 'open')
-            return
+        const colorBytes=new Uint8Array(3)
 
         for (let x = 0; x < this.width; x++) {
             for (let y = 0; y < this.height; y++) {
 
                 const color = this.frameBuffer[x][y]
-                let rgb = `${(~~color.r).toString(16).padStart(2, '0')}${(~~color.g).toString(16).padStart(2, '0')}${(~~color.b).toString(16).padStart(2, '0')}`
 
-                // console.log(rgb)
-                // if (rgb=='000000')
-                //     rgb='555555'
-
-                const rgbArray = encoder.encode(rgb)
+                colorBytes[0]=color.r
+                colorBytes[1]=color.g
+                colorBytes[2]=color.b
 
                 // console.log(this.frameBuffOffsets.length)
                 for (const offset of this.sendBufferOffsets[~~x][~~y]) {
-                    this.sendBuffer.set(rgbArray, offset)
-
-
+                    this.sendBuffer.set(colorBytes, offset)
                 }
                 color.reset()
 
             }
         }
 
-        if (!this.flood)
-            this.send()
+        this.send()
 
     }
 
