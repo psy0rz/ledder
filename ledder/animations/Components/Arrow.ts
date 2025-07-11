254cee63d0f3b277033c3bc26cb8673fd955358a
Edwin Eefting <edwin@datux.nl>
2025-07-12 00:01:39 +0200

    arrow wip

--- ledder/animations/Components/Arrow.ts
+++ ledder/animations/Components/Arrow.ts
@@ -9,16 +9,74 @@ import Animator from "../../Animator.js"
 import Pixel from "../../Pixel.js";
 import Color from "../../Color.js";
 
+import DrawLine from "../../draw/DrawLine.js";
 
 export default class Arrow extends  Animator
 {
 
     async run(box: PixelBox, scheduler: Scheduler, controls: ControlGroup) {
 
-        const pixel=new Pixel(0,0, new Color(255,0,0))
-        box.add(pixel)
-        box.xMin
-        box.yMax
+
+        let colorControl = controls.color()
+
+            let direction= controls.select("Direction", "up",[
+            {
+                "id":"up",
+                "name":"Up",
+            },
+            {
+                "id":"down",
+                "name":"Down",
+            },
+            {
+                "id":"left",
+                "name":"Left",
+            },
+            {
+                "id":"right",
+                "name":"Right",
+            }
+        ], true)
+
+        let maxOffset= box.xMax-box.xMin
+
+        let offset = controls.value("Wall offset", 3, 0, maxOffset, 1,true)
+        let startOffset = controls.value("Start offset", 1, 0, maxOffset, 1,true)
+        let endOffset = controls.value("End offset", 1, 0, maxOffset, 1,true)
+        let size = controls.value("Size", 3, 1, 10, 1,true)
+
+
+        let startX=0
+        let startY=0
+
+        //end is where the array head is
+        let endX=0
+        let endY=0
+
+        //flank end coordinates (line will be drawn from endX,Y to flankX,Y)
+        let flank1X=0
+        let flank1Y=0
+        let flank2X=0
+        let flank2Y=0
+
+
+        if (direction.selected === "up") {
+            startX=endX=box.xMin+offset.value
+            startY=box.yMax-startOffset.value
+            endY=box.yMin+endOffset.value
+
+            flank1X=endX+size.value
+            flank2X=endX-size.value
+            flank1Y=flank2Y=endY+size.value
+        }
+
+        //main line
+        box.add(new DrawLine(startX, startY, endX, endY, colorControl))
+
+        //arrow flanks
+        box.add(new DrawLine(endX, endY, flank1X, flank1Y, colorControl))
+        box.add(new DrawLine(endX, endY, flank2X, flank2Y, colorControl))
+
 
     }
 }
