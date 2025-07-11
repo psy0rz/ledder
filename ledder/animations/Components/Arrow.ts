e26b07df884cf8d604c66222b7e4b1815718c327
Edwin Eefting <edwin@datux.nl>
2025-07-11 22:00:16 +0200

    cleanup

---
+++ ledder/animations/Components/Arrow.ts
@@ -0,0 +1,20 @@
+import PixelBox from "../PixelBox.js"
+import Pixel from "../Pixel.js"
+import Scheduler from "../Scheduler.js"
+import Color from "../Color.js"
+import ControlGroup from "../ControlGroup.js"
+import Animator from "../Animator.js"
+
+
+export default class Arrow extends  Animator
+{
+
+    async run(box: PixelBox, scheduler: Scheduler, controls: ControlGroup) {
+
+        const pixel=new Pixel(0,0, new Color(255,0,0))
+        box.add(pixel)
+        box.xMin
+        box.yMax
+
+    }
+}
