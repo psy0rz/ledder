f67574cecf508ef92d79157377b4b36c7174ffb9
DatuX <edwin@datux.nl>
2025-10-31 22:48:19 +0100

    flexible angle

--- ledder/animations/Components/Arrow.ts
+++ ledder/animations/Components/Arrow.ts
@@ -1,105 +1,57 @@
 import PixelBox from "../../PixelBox.js"
 import Scheduler from "../../Scheduler.js"
 import ControlGroup from "../../ControlGroup.js"
-import PixelList from "../../PixelList.js"
-import DrawAsciiArt from "../../draw/DrawAsciiArt.js"
-import FxMovie from "../../fx/FxMovie.js"
-import FxRotate from "../../fx/FxRotate.js"
 import Animator from "../../Animator.js"
-import Pixel from "../../Pixel.js";
-import Color from "../../Color.js";
-
 import DrawLine from "../../draw/DrawLine.js";
 import FxColorPattern from "../../fx/FxColorPattern.js";
-import FxColorCycle from "../../fx/FxColorCycle.js";
 
 export default class Arrow extends Animator {
-
     async run(box: PixelBox, scheduler: Scheduler, controls: ControlGroup) {
+        // User specifies tip (x, y), angle, arrow length, flank length, flank angle
+        let colorControl = controls.color('Color', 255,255,0,1);
+        let patGroup = controls.group("Color pattern", true, false, true);
+        let pat = new FxColorPattern(scheduler, patGroup, 240, 10, false, true, 'Bertrik fire');
 
+        // Controls for tip position
+        let tipX = controls.value("Tip X", Math.round((box.xMin + box.xMax) / 2), box.xMin, box.xMax, 1, true);
+        let tipY = controls.value("Tip Y", Math.round((box.yMin + box.yMax) / 2), box.yMin, box.yMax, 1, true);
+        let angle = controls.value("Angle", 0, 0, 360, 1, true);
+        // Arrow and flank lengths
+        let arrowLength = controls.value("Arrow length", 10, 1, Math.max(box.xMax - box.xMin, box.yMax - box.yMin), 1, true);
+        let flankLength = controls.value("Flank length", 5, 1, 20, 1, true);
+        let flankAngle = controls.value("Flank angle", 35, 5, 85, 1, true); // degrees from main line
 
-        let colorControl = controls.color()
+        // Convert angles to radians
+        let theta = angle.value * Math.PI / 180;
+        let flankTheta1 = (angle.value + flankAngle.value) * Math.PI / 180;
+        let flankTheta2 = (angle.value - flankAngle.value) * Math.PI / 180;
 
-        let patGroup = controls.group("Color pattern", true,false,true)
-        // controls.disable(patGroup)
-        let pat = new FxColorPattern(scheduler, patGroup, 240, 10, false, true, 'Bertrik fire')
+        // Calculate base of arrow (start of main line)
+        let baseX = Math.round(tipX.value - Math.cos(theta) * arrowLength.value);
+        let baseY = Math.round(tipY.value - Math.sin(theta) * arrowLength.value);
 
+        // Calculate flank endpoints
+        let flank1X = Math.round(tipX.value - Math.cos(flankTheta1) * flankLength.value);
+        let flank1Y = Math.round(tipY.value - Math.sin(flankTheta1) * flankLength.value);
+        let flank2X = Math.round(tipX.value - Math.cos(flankTheta2) * flankLength.value);
+        let flank2Y = Math.round(tipY.value - Math.sin(flankTheta2) * flankLength.value);
 
-        let direction = controls.select("Direction", "up", [
-            {
-                "id": "up",
-                "name": "Up",
-            },
-            {
-                "id": "down",
-                "name": "Down",
-            },
-            {
-                "id": "left",
-                "name": "Left",
-            },
-            {
-                "id": "right",
-                "name": "Right",
-            }
-        ], true)
-
-        let maxOffset = box.xMax - box.xMin
-
-        let offset = controls.value("Wall offset", 3, 0, maxOffset, 1, true)
-        let startOffset = controls.value("Start offset", 1, 0, maxOffset, 1, true)
-        let endOffset = controls.value("End offset", 1, 0, maxOffset, 1, true)
-        let size = controls.value("Size", 3, 1, 10, 1, true)
-
-
-        let startX = 0
-        let startY = 0
-
-        //end is where the arrow head is
-        let endX = 0
-        let endY = 0
-
-        //flank end coordinates (line will be drawn from endX,Y to flankX,Y)
-        let flank1X = 0
-        let flank1Y = 0
-        let flank2X = 0
-        let flank2Y = 0
-
-
-        if (direction.selected === "up") {
-            startX = endX = box.xMin + offset.value
-            startY = box.yMax - startOffset.value
-            endY = box.yMin + endOffset.value
-
-            flank1X = endX + size.value
-            flank2X = endX - size.value
-            flank1Y = flank2Y = endY + size.value
-        }
-
-
-        //main line
-        // box.add(new DrawLine(startX, startY, endX, endY, colorControl))
-        let mainLine = new DrawLine(startX, startY, endX, endY, colorControl)
-        box.add(mainLine)
-
-        //arrow flanks
-        let flank1 = new DrawLine(flank1X, flank1Y, endX, endY, colorControl)
-        box.add(flank1)
-
-        let flank2 = new DrawLine(flank2X, flank2Y, endX, endY, colorControl)
-        box.add(flank2)
+        // Draw main arrow line (from base to tip)
+        let mainLine = new DrawLine(baseX, baseY, tipX.value, tipY.value, colorControl);
+        box.add(mainLine);
+        // Draw flanks (from tip to each flank endpoint)
+        let flank1 = new DrawLine(tipX.value, tipY.value, flank1X, flank1Y, colorControl);
+        box.add(flank1);
+        let flank2 = new DrawLine(tipX.value, tipY.value, flank2X, flank2Y, colorControl);
+        box.add(flank2);
 
         if (patGroup.enabled) {
-           controls.disable(colorControl)
-            pat.run(mainLine)
-            pat.run(flank1)
-            pat.run(flank2)
+            controls.disable(colorControl);
+            pat.run(mainLine);
+            pat.run(flank1);
+            pat.run(flank2);
+        } else {
+            controls.enable(colorControl);
         }
-        else
-        {
-           controls.enable(colorControl)
-        }
-
-
     }
 }
