5ecf400b5440efd03d07dbfbbb645d2d54e38eaf
DatuX <edwin@datux.nl>
2026-08-15 20:30:51 +0200

    much more efficent preview rebuilding

--- ledder/server/buildpreviews.ts
+++ ledder/server/buildpreviews.ts
@@ -4,8 +4,12 @@
 import {presetStore} from "./PresetStore.js"
 import {previewStore} from "./PreviewStore.js"
 
+//normally only outdated previews are rendered (decided by content hash, see PreviewCache).
+//-f/--force rerenders all of them, e.g. when something outside the hashed sources changed.
+const forceRebuild = process.argv.includes('-f') || process.argv.includes('--force')
+
 await presetStore.storeAnimationPresetList()
-await previewStore.renderAll(presetStore.animationPresetList, process.argv.indexOf('-f')!=-1)
+await previewStore.renderAll(presetStore.animationPresetList, forceRebuild)
 
 //force exit, in case of async hanging stuff
 process.exit(0)
